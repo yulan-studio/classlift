114bea5efacd8d11f7f51aa0cd0ab80b5a46027d
Yulan <qiangyulan@hotmail.com>
2026-06-21 08:00:39 -0400

    Add dashboard

---
+++ classlift_platform/Billing/ViewModels/BillingDashboardViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Billing.ViewModels
+{
+    public class BillingDashboardViewModel
+    {
+        public decimal TotalRevenue { get; set; }
+        public decimal PendingAmount { get; set; }
+        public decimal OverdueAmount { get; set; }
+
+        public int TotalOrganizations { get; set; }
+        public int ActiveSubscriptions { get; set; }
+
+        public int PendingInvoices { get; set; }
+        public int PaidInvoices { get; set; }
+        public int OverdueInvoices { get; set; }
+
+        public List<PlanCountItem> PlanCounts { get; set; } = new();
+    }
+
+    public class PlanCountItem
+    {
+        public string PlanName { get; set; } = "";
+        public int Count { get; set; }
+    }
+}
