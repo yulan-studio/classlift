4079d6e3244362f768d176f4aca575549e264091
Yulan <qiangyulan@hotmail.com>
2026-06-21 17:38:12 -0400

    Add organizations view

---
+++ classlift_platform/Billing/Controllers/OrganizationsController.cs
@@ -0,0 +1,41 @@
+﻿using Billing.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Billing.Controllers
+{
+    [Route("[controller]")]
+    public class OrganizationsController : Controller
+    {
+        private readonly BillingDbContext _context;
+
+        public OrganizationsController(BillingDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Index()
+        {
+            var organizations = await _context.Organizations
+                .Include(o => o.CurrentPlan)
+                .OrderBy(o => o.OrganizationName)
+                .ToListAsync();
+
+            return View(organizations);
+        }
+
+        [HttpGet("Details/{id:int}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var organization = await _context.Organizations
+                .Include(o => o.CurrentPlan)
+                .FirstOrDefaultAsync(o => o.OrganizationId == id);
+
+            if (organization == null)
+                return NotFound();
+
+            return View(organization);
+        }
+    }
+}
