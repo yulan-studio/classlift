3b3cbc72118201ae92f82f58c0fad4942fd443b2
Yulan <qiangyulan@hotmail.com>
2026-06-22 22:28:41 -0400

    Add Feature Enforcement - basic version with query-string organizationId

--- classlift_platform/Billing/Constants/FeatureKeys.cs
+++ classlift_platform/Billing/Constants/FeatureKeys.cs
@@ -2,8 +2,8 @@
 {
     public static class FeatureKeys
     {
-        public const string CourseManagement =
-           "course_student_management";
+        public const string TeacherCourseStudentManagement =
+           "teacher_course_student_management";
 
         public const string AttendanceTracking =
             "attendance_tracking";
@@ -22,5 +22,19 @@
 
         public const string AiEnhancements =
             "ai_enhancements";
+
+        public const string GroupSessionScheduling = 
+            "group_session_scheduling";
+
+        public const string OneOnOneSessionScheduling = 
+            "one_on_one_session_scheduling";
+
+        public const string PerCoursePayment = 
+            "per_course_payment";
+
+        public const string PrepaidPerHourPayment = 
+            "prepaid_per_hour_payment";
+
+
     }
 }
---
+++ classlift_platform/Billing/Filters/RequireFeatureAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Billing.Filters
+{
+    public class RequireFeatureAttribute : TypeFilterAttribute
+    {
+        public RequireFeatureAttribute(string featureKey)
+            : base(typeof(RequireFeatureFilter))
+        {
+            Arguments = new object[] { featureKey };
+        }
+    }
+}
