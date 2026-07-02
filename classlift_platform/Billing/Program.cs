226e1f99a1a67f4c505db3c4be4a71254efde3e8
Yulan <qiangyulan@hotmail.com>
2026-07-02 01:57:50 -0400

    Change Program.cs for Railway

--- classlift_platform/Billing/Program.cs
+++ classlift_platform/Billing/Program.cs
@@ -14,7 +14,7 @@ using MySqlConnector;
 var builder = WebApplication.CreateBuilder(args);
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-//builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 builder.Services.AddControllersWithViews();
 
