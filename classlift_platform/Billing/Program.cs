bf024ec048f065af0ee340f49b50a90adf05dddd
Yulan <qiangyulan@gmail.com>
2026-08-03 14:59:41 -0400

    remove registration link in classlift_courses

--- classlift_platform/Billing/Program.cs
+++ classlift_platform/Billing/Program.cs
@@ -52,7 +52,7 @@ builder.Services.AddAuthorization();
 
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+//builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var dbHost = builder.Configuration["TenantDatabase:Host"];
 var dbPort = builder.Configuration["TenantDatabase:Port"];
